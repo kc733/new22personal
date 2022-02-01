e2b3563f8f70a5a5f09a2a28b9e11c0215be9800
Mo <86203212+kc733@users.noreply.github.com>
2022-02-01 19:27:26 +0200

    new small program for studying chemistry.

--- newjava2022/randomExecesises/chemistryRandom.java
+++ newjava2022/randomExecesises/chemistryRandom.java
@@ -1,18 +1,33 @@
+// This is a program to show random names from home chemistry, that we need to study in school
+// I use it to memorise their formulas
+
 import java.util.Random;
+import javax.swing.JOptionPane;
 
 public class chemistryRandom {
 
     public static void main(String[] args) {
         
+        // Make a sting for random result
         String currentChem;
-        String[] homeChem = {"Soda", "Can", "Wtf is happening"};
 
+        // Make an array with names to show
+        String[] homeChem = {"Soda", "Perekis vodoroda", "Uksus", "Margantsovka", "Sahar", "Sol' kuhonnaya",
+        "Krahmal", "Spirt", "Nashatirniy Spirt", "Glicerin", "Aceton", "Uglekisliy gaz", "Chabniy gaz",
+        "Metanol", "Polietilen", "Metan"}; // I couldn't use russian letters, the build fails
+
+        // Make random object/variable
         Random randomChem = new Random();
 
+        // Make your end result a random name from your array
         currentChem = homeChem [randomChem.nextInt(homeChem.length)];
 
+        // Show random result in a message dialog with JOptionPane
+        JOptionPane.showMessageDialog(null, currentChem);
+        
         System.out.println(currentChem);
 
+        // It works!! :)
 
-}
+    }
 }
