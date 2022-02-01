8fe192fcc2b7f067095bcdf8c6a5ab3435df1927
Mo <86203212+kc733@users.noreply.github.com>
2022-02-01 18:57:50 +0200

    reupload to new rep

---
+++ newjava2022/randomExecesises/chemistryRandom.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class chemistryRandom {
+
+    public static void main(String[] args) {
+        
+        String currentChem;
+        String[] homeChem = {"Soda", "Can", "Wtf is happening"};
+
+        Random randomChem = new Random();
+
+        currentChem = homeChem [randomChem.nextInt(homeChem.length)];
+
+        System.out.println(currentChem);
+
+
+}
+}
